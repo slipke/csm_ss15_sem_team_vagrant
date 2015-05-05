ce366d3da3d4d13225862a0a988ea4cf34088c74
k ko <kk114@hdm-stuttgart.de>
2015-05-05 14:20:00 +0200

    tomcat installation with management user

---
+++ java-tomcat/chef/metadata.rb
@@ -0,0 +1,13 @@
+name             'tomcat-chef'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tomcat'
+long_description 'Installs/Configures tomcat'
+
+version          '0.1.0'
+
+depends "apt"
+depends "java"
+depends 'tomcat'
+
---
+++ java-tomcat/chef/roles/tomcatConfig.rb
@@ -0,0 +1,6 @@
+name "tomcatConfig"
+description "Baseline configuration for all systems."
+ 
+run_list(
+    #'recipe[tomcat-chef::default]'
+	)
