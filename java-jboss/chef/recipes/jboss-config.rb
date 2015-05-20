b12488715a3336f89b5a34ea83b97590a447c095
k ko <kk114@hdm-stuttgart.de>
2015-05-20 11:56:41 +0200

    added manual start of the jboss instance, because the service jboss start command won't succeed

--- java-jboss/chef/recipes/jboss-config.rb
+++ java-jboss/chef/recipes/jboss-config.rb
@@ -1,14 +1,15 @@
 template "jbossmgmt-users.properties." do
+  Chef::Log.info "Add user with Config template"
   path "/opt/jboss/standalone/configuration/mgmt-users.properties"
   source "jboss.mgmt-users.properties.erb"
-  #owner "web"
-  #group "web"
+  owner "web"
+  group "web"
   mode "0644"
   notifies :enable, 'service[jboss]', :delayed
   notifies :restart, 'service[jboss]', :delayed
- # notifies :restart, resources(:service => "jboss")
 end
 
-service "jboss" do
-  action :start 
+execute "start jboss server manually" do
+  command "sudo /opt/jboss/bin/standalone.sh"
+  action :run
 end
